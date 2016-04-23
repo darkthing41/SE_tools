482bc4efaa5b81fba7958b6e464658f507cf5528
Charlie <darkthing41@gmail.com>
2016-04-23 19:09:10 +0100

    Base Template
    - Template for a new script to be used in Monodev

---
+++ Template.cs
@@ -0,0 +1,68 @@
+#region Header
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage;
+using VRage.Game.ModAPI.Ingame;
+using VRageMath;
+
+namespace SpaceEngineersScripting
+{
+	public class Wrapper
+	{
+		static void Main()
+		{
+			new Program().Main ("");
+		}
+	}
+
+
+	class Program : Sandbox.ModAPI.Ingame.MyGridProgram
+	{
+#endregion
+		#region CodeEditor
+		//Configuration
+		//--------------------
+
+
+		//Definitions
+		//--------------------
+
+
+		//Internal Types
+		//--------------------
+
+
+		//Global variables
+		//--------------------
+
+
+		//Program
+		//--------------------
+
+		public Program()
+		{
+
+		}
+
+
+		public void Save()
+		{
+
+		}
+
+
+		public void Main(string argument)
+		{
+
+		}
+		#endregion
+#region footer
+	}
+}
+#endregion
